e8190046ed3a0c346a85f42cce2b0acbe5c446a2
myasko <komondor25@gmail.com>
2026-03-25 01:15:48 +0300

    added strategies functions to use in SbrScraper

---
+++ src/file_loading/scraper/sbr_scraper/strategies.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from playwright.sync_api import Locator
+
+from src.file_loading.functions.extract_date_from_text import extract_date_from_text
+
+
+def get_date_from_referenceable(locator: Locator) -> datetime | None:
+    locator = locator.locator("xpath=../../../../..").locator(".document-regular_date")
+    date_str = locator.text_content()
+    date = extract_date_from_text(date_str)
+    return date
+
+
+def get_date_from_versions_item(locator: Locator) -> datetime | None:
+    date_str = locator.get_attribute("data-tooltip-content")
+    date = extract_date_from_text(date_str)
+    return date
