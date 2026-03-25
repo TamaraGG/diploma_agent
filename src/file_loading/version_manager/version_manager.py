e445faf73d592dd2587645cd97f6fde1108ea7f9
myasko <komondor25@gmail.com>
2026-03-25 03:55:08 +0300

    added get_last_common_month method

--- src/file_loading/version_manager/version_manager.py
+++ src/file_loading/version_manager/version_manager.py
@@ -1,17 +1,40 @@
 from datetime import datetime
 
-from src.file_loading.models.models import Document
+from src.file_loading.models.models import Document, DocumentVersion
 from src.file_loading.version_manager.base_version_manager import BaseVersionManager
 
 
 class VersionManager(BaseVersionManager):
     @staticmethod
-    def get_latest_version(documents: list[Document]) -> datetime | None:
+    def get_latest_version(documents: list[Document]) -> list[DocumentVersion] | None:
         if not documents:
             return None
 
-        same_versions: set[datetime] = set(documents.pop().versions.items())
+        return [doc.versions[0] for doc in documents]
+
+    @staticmethod
+    def get_last_common_month(documents: list[Document]) -> list[DocumentVersion]:
+        if not documents:
+            return []
+
+        date_sets = [{datetime(dd.load_date.year, dd.load_date.month, 1)
+                      for dd in doc.versions}
+                      for doc in documents]
+        common_month = date_sets[0]
+        for ds in date_sets:
+            common_month = common_month.intersection(ds)
+            if not common_month:
+                break
+
+        target_date = sorted(list(common_month), reverse=True)[0]
+
+        doc_versions = []
         for document in documents:
-            same_versions.intersection(document.versions.items())
-        sorted(same_versions, reverse=True)
-        return same_versions.pop() if same_versions else None
+            prev_version = document.versions[0]
+            for version in document.versions[1:]:
+                if version.load_date < target_date:
+                    doc_versions.append(prev_version)
+                    break
+                prev_version = version
+
+        return doc_versions
